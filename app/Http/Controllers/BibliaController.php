87b51acad486dad9bcb6f7e355d9fd12b3ee54fc
vitorfacalha <vitor.facalha@aluno.ifsp.edu.br>
2024-12-05 03:46:49 -0300

    update controllers

--- app/Http/Controllers/BibliaController.php
+++ app/Http/Controllers/BibliaController.php
@@ -1,10 +1,39 @@
 <?php
 
+// app/Http/Controllers/BibliaController.php
+
 namespace App\Http\Controllers;
 
+use App\Models\Testamento;
+use App\Models\Capitulo;
 use Illuminate\Http\Request;
 
 class BibliaController extends Controller
 {
-    //
+    public function index()
+    {
+        // Exibir todos os testamentos
+        $testamentos = Testamento::all();
+        return view('biblia.index', compact('testamentos'));
+    }
+
+    public function livros($testamentoId)
+    {
+        // Exibir livros (testamentos)
+        $testamento = Testamento::find($testamentoId);
+        $capitulos = Capitulo::where('testamento_id', $testamentoId)->get();
+        return view('biblia.livros', compact('testamento', 'capitulos'));
+    }
+
+    public function buscar(Request $request)
+    {
+        $termo = $request->input('termo');
+        $capitulos = Capitulo::where('texto', 'LIKE', "%$termo%")
+            ->orWhereHas('testamento', function ($query) use ($termo) {
+                $query->where('nome', 'LIKE', "%$termo%")
+                      ->orWhere('abrev', 'LIKE', "%$termo%");
+            })->get();
+        return view('biblia.buscar', compact('capitulos'));
+    }
 }
+
--- app/Http/Controllers/FavoritoController.php
+++ app/Http/Controllers/FavoritoController.php
@@ -1,10 +1,29 @@
 <?php
 
+// app/Http/Controllers/FavoritoController.php
+
 namespace App\Http\Controllers;
 
+use App\Models\Capitulo;
+use App\Models\Favorito;
 use Illuminate\Http\Request;
 
 class FavoritoController extends Controller
 {
-    //
+    public function adicionarFavorito($capituloId)
+    {
+        $capitulo = Capitulo::find($capituloId);
+        $favorito = new Favorito();
+        $favorito->capitulo_id = $capitulo->id;
+        $favorito->save();
+
+        return back();
+    }
+
+    public function listarFavoritos()
+    {
+        $favoritos = Favorito::with('capitulo')->get();
+        return view('favoritos.index', compact('favoritos'));
+    }
 }
+
